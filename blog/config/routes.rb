e61baaa980e92a4be9a9c9a08287fae3ad468982
Malak Nasser <malaknasser26@gmail.com>
2024-05-16 17:51:13 +0300

    Add day4

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,6 +7,12 @@ Rails.application.routes.draw do
   root "articles#index"
   resources :articles do
     resources :comments
+  end
+
+  resources :articles do
+    member do
+      post 'report'
+    end
   end
   # get "up" => "rails/health#show", as: :rails_health_check
 
